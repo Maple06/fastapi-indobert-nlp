5f00cdd4923f13f3eb4002c6c63fa5eff0baf971
MapleExists <41141087+Maple06@users.noreply.github.com>
2023-04-06 09:34:38 +0700

    fix typo

--- app/api/load_models.py
+++ app/api/load_models.py
@@ -117,7 +117,7 @@ class NLPIndoBert:
         else:
             logger.info(f"Using device: {platform.processor()}")
 
-        # Defining optimizerd
+        # Defining optimizer
         self.optimizer = AdamW(self.model.parameters(), lr = 1e-5)
 
         # Computing class weights
